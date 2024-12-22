7a931ad12b0426024b29516d41bedb542794f1d9
keiseiisobe <keisei.isobe@gmail.com>
2024-12-22 13:02:29 +0900

    modified wordpress and mariadb

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
-mysqld --skip-grant-tables&
+mariadbd&
+sleep 10
 
-mysql -u root -p password -e "CREATE DATABASE ${MYSQL_DATABASE};"
-mysql -u root -p password -e "CREATE USER ${MYSQL_USER}@mariadb IDENTIFIED BY ${MYSQL_PASSWORD};"
-mysql -u root -p password -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${MYSQL_USER}@mariadb;"
-mysql -u root -p password -e "FLUSH PRIVILEGES;"
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';"
+
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+
+/etc/init.d/mariadb stop
+sleep 10
 
 exec "$@"
