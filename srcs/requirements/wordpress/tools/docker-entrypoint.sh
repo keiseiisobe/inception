28c09f9464573ad585eb6369b4208d32c27b6c84
keiseiisobe <keisei.isobe@gmail.com>
2025-01-09 15:52:09 +0900

    fixed wordpress default page

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+sleep 10
+
 if [ ! -s wp-config.php ]; then
     wp --allow-root core download
     wp --allow-root config create \
@@ -11,8 +13,12 @@ if [ ! -s wp-config.php ]; then
        --url=${WORDPRESS_URL} \
        --title=${WORDPRESS_TITLE} \
        --admin_user=${WORDPRESS_ADMIN_USER} \
+       --admin_password=$(< ${WORDPRESS_ADMIN_PASSWORD_FILE})  \
        --admin_email=${WORDPRESS_ADMIN_EMAIL}
-    wp --allow-root user create ${WORDPRESS_USER_LOGIN} ${WORDPRESS_USER_EMAIL}
+    wp --allow-root user create \
+	${WORDPRESS_USER_LOGIN} \
+	${WORDPRESS_USER_EMAIL} \
+	--user_pass=$(< ${WORDPRESS_USER_PASSWORD_FILE})
 
     # redis
     wp --allow-root plugin install redis-cache --activate
